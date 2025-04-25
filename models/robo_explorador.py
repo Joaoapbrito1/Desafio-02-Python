d9a5fbe0b6baa2c64f5d1dec343b7a62afc677d7
Joaoapbrito1 <joao.brito@zup.com.br>
2025-04-25 14:28:56 -0300

    feat: add .gitignore.

--- models/robo_explorador.py
+++
@@ -1,8 +0,0 @@
-class RoboExplorador:
-    def __init__(self, nome: str, planeta_destino: str, energia: int):
-        self.nome = nome
-        self.planeta_destino = planeta_destino
-        self.energia = energia
-
-    def __str__(self):
-        return f"Robô {self.nome} - Destino: {self.planeta_destino} - Energia: {self.energia}%"
